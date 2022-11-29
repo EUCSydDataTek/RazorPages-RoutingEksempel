1dc9fe7c9e783100bcfc239300ecd8a142d52c68
jako5457 <jako5457@gmail.com>
2022-11-29 08:39:01 +0100

    opdateret til .net 7.0

--- RoutingEksempel/Program.cs
+++ RoutingEksempel/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -29,9 +30,28 @@ else
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
+//THE ROUTING ZONE
 app.UseRouting();
 
+app.Use(next => context =>
+{
+    var endpoint = context.GetEndpoint();
+
+    if (endpoint is null)
+    {
+        return next(context);
+    }
+
+    return next(context);
+});
+
 app.UseAuthorization();
+
+app.MapGet("/map/hello/{name}", async context =>
+{
+    var name = context.Request.RouteValues["name"];
+    await context.Response.WriteAsync($"Hello {name}!");
+});
 
 app.MapRazorPages();
 
