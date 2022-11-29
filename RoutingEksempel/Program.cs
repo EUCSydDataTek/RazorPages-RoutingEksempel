37331987f7efa88cdb2a0575223fc0b6da29faf1
jako5457 <jako5457@gmail.com>
2022-11-29 08:49:23 +0100

    tilføjet implicit usings

--- RoutingEksempel/Program.cs
+++ RoutingEksempel/Program.cs
@@ -1,14 +1,3 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 var builder = WebApplication.CreateBuilder();
 
 builder.Services.AddRazorPages();
